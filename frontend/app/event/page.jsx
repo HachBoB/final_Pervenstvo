95af85dbd9ed3c1459c161e3ae784ac78b676026
vtiny@inbox.ru <vtiny@inbox.ru>
2024-12-08 01:40:44 +0400

    йоу йоу

---
+++ frontend/app/event/page.jsx
@@ -0,0 +1,7 @@
+export default function event() {
+    return(
+        <div>
+            Event
+        </div>
+    );
+}
--- frontend/app/personal/page.jsx
+++ frontend/app/personal/page.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import ImageUpload from "@/app/personal/ClientComponent";
 
 export default function Personal() {
   return (
